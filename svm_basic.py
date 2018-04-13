0cc464583da11a82006c850e64a0f6d4b49d1a39
PriyamPatel <pjpatel3@dons.usfca.edu>
2018-04-12 21:51:09 -0700

    lab 2 done

--- svm_basic.py
+++ svm_basic.py
@@ -1,6 +1,32 @@
 from numpy import *
 from time import sleep
 
+def loadDataSet(fileName):
+    dataMat = []; labelMat = []
+    fr = open(fileName)
+    for line in fr.readlines():
+        lineArr = line.strip().split(',')
+        dataMat.append([float(lineArr[0]), float(lineArr[1])])
+        labelMat.append(float(lineArr[2]))
+    return dataMat,labelMat
+
+def selectJrand(i,m):
+    j=i #we want to select any J not equal to i
+    while (j==i):
+        j = int(random.uniform(0,m))
+    return j
+
+def clipAlpha(aj,H,L):
+    if aj > H: 
+        aj = H
+    if L > aj:
+        aj = L
+    return aj
+
+'''#######********************************
+Non-Kernel VErsions below
+'''#######********************************
+
 class optStructK:
     def __init__(self,dataMatIn, classLabels, C, toler):  # Initialize the structure with the parameters 
         self.X = dataMatIn
@@ -11,110 +37,212 @@ class optStructK:
         self.alphas = mat(zeros((self.m,1)))
         self.b = 0
         self.eCache = mat(zeros((self.m,2))) #first column is valid flag
+        
+def calcEkK(oS, k):
+    fXk = float(multiply(oS.alphas,oS.labelMat).T*(oS.X*oS.X[k,:].T)) + oS.b
+    Ek = fXk - float(oS.labelMat[k])
+    return Ek
+        
+def selectJK(i, oS, Ei):         #this is the second choice -heurstic, and calcs Ej
+    maxK = -1; maxDeltaE = 0; Ej = 0
+    oS.eCache[i] = [1,Ei]  #set valid #choose the alpha that gives the maximum delta E
+    validEcacheList = nonzero(oS.eCache[:,0].A)[0]
+    if (len(validEcacheList)) > 1:
+        for k in validEcacheList:   #loop through valid Ecache values and find the one that maximizes delta E
+            if k == i: continue #don't calc for i, waste of time
+            Ek = calcEkK(oS, k)
+            deltaE = abs(Ei - Ek)
+            if (deltaE > maxDeltaE):
+                maxK = k; maxDeltaE = deltaE; Ej = Ek
+        return maxK, Ej
+    else:   #in this case (first time around) we don't have any valid eCache values
+        j = selectJrand(i, oS.m)
+        Ej = calcEkK(oS, j)
+    return j, Ej
 
+def updateEkK(oS, k):#after any alpha has changed update the new value in the cache
+    Ek = calcEkK(oS, k)
+    oS.eCache[k] = [1,Ek]
+        
+def innerLK(i, oS):
+    Ei = calcEkK(oS, i)
+    if ((oS.labelMat[i]*Ei < -oS.tol) and (oS.alphas[i] < oS.C)) or ((oS.labelMat[i]*Ei > oS.tol) and (oS.alphas[i] > 0)):
+        j,Ej = selectJK(i, oS, Ei) #this has been changed from selectJrand
+        alphaIold = oS.alphas[i].copy(); alphaJold = oS.alphas[j].copy();
+        if (oS.labelMat[i] != oS.labelMat[j]):
+            L = max(0, oS.alphas[j] - oS.alphas[i])
+            H = min(oS.C, oS.C + oS.alphas[j] - oS.alphas[i])
+        else:
+            L = max(0, oS.alphas[j] + oS.alphas[i] - oS.C)
+            H = min(oS.C, oS.alphas[j] + oS.alphas[i])
+        if L==H: 
+#             print("L==H"); 
+            return 0
+        eta = 2.0 * oS.X[i,:]*oS.X[j,:].T - oS.X[i,:]*oS.X[i,:].T - oS.X[j,:]*oS.X[j,:].T
+        if eta >= 0: 
+#             print("eta>=0"); 
+            return 0
+        oS.alphas[j] -= oS.labelMat[j]*(Ei - Ej)/eta
+        oS.alphas[j] = clipAlpha(oS.alphas[j],H,L)
+        updateEkK(oS, j) #added this for the Ecache
+        if (abs(oS.alphas[j] - alphaJold) < 0.00001): 
+#             print("j not moving enough"); 
+            return 0
+        oS.alphas[i] += oS.labelMat[j]*oS.labelMat[i]*(alphaJold - oS.alphas[j])#update i by the same amount as j
+        updateEkK(oS, i) #added this for the Ecache                    #the update is in the oppostie direction
+        b1 = oS.b - Ei- oS.labelMat[i]*(oS.alphas[i]-alphaIold)*oS.X[i,:]*oS.X[i,:].T - oS.labelMat[j]*(oS.alphas[j]-alphaJold)*oS.X[i,:]*oS.X[j,:].T
+        b2 = oS.b - Ej- oS.labelMat[i]*(oS.alphas[i]-alphaIold)*oS.X[i,:]*oS.X[j,:].T - oS.labelMat[j]*(oS.alphas[j]-alphaJold)*oS.X[j,:]*oS.X[j,:].T
+        if (0 < oS.alphas[i]) and (oS.C > oS.alphas[i]): oS.b = b1
+        elif (0 < oS.alphas[j]) and (oS.C > oS.alphas[j]): oS.b = b2
+        else: oS.b = (b1 + b2)/2.0
+        return 1
+    else: return 0
+
+def smoPK(dataMatIn, classLabels, C, toler, maxIter):    #full Platt SMO
+    oS = optStructK(mat(dataMatIn),mat(classLabels).transpose(),C,toler)
+    iter = 0
+    entireSet = True; alphaPairsChanged = 0
+    while (iter < maxIter) and ((alphaPairsChanged > 0) or (entireSet)):
+        alphaPairsChanged = 0
+        if entireSet:   #go over all
+            for i in range(oS.m):        
+                alphaPairsChanged += innerLK(i,oS)
+#                 print("fullSet, iter: %d i:%d, pairs changed %d" % (iter,i,alphaPairsChanged))
+            iter += 1
+        else:#go over non-bound (railed) alphas
+            nonBoundIs = nonzero((oS.alphas.A > 0) * (oS.alphas.A < C))[0]
+            for i in nonBoundIs:
+                alphaPairsChanged += innerLK(i,oS)
+#                 print("non-bound, iter: %d i:%d, pairs changed %d" % (iter,i,alphaPairsChanged))
+            iter += 1
+        if entireSet: entireSet = False #toggle entire set loop
+        elif (alphaPairsChanged == 0): entireSet = True  
+#         print("iteration number: %d" % iter)
+    return oS.b,oS.alphas
+
+def calcWs(alphas,dataArr,classLabels):
+    X = mat(dataArr); labelMat = mat(classLabels).transpose()
+    m,n = shape(X)
+    w = zeros((n,1))
+    for i in range(m):
+        w += multiply(alphas[i]*labelMat[i],X[i,:].T)
+    return w
+
+def plot_fit(fit_line, datamatrix, labelmatrix):
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    weights = fit_line
+
+    dataarray = np.asarray(datamatrix)
+    n = dataarray.shape[0]
+
+    # Keep track of the two classes in different arrays so they can be plotted later...
+    xcord1 = []
+    ycord1 = []
+    xcord2 = []
+    ycord2 = []
+    for i in range(n):
+        if int(labelmatrix[i]) == 1:
+            xcord1.append(dataarray[i, 0])
+            ycord1.append(dataarray[i, 1])
+        else:
+            xcord2.append(dataarray[i, 0])
+            ycord2.append(dataarray[i, 1])
+    fig = plt.figure()
+
+    # Plot the data as points with different colours
+    ax = fig.add_subplot(111)
+    ax.scatter(xcord1, ycord1, s=30, c='red', marker='s')
+    ax.scatter(xcord2, ycord2, s=30, c='green')
+
+    # Plot the best-fit line
+    x = np.arange(-2.0, 7.5, 0.1)
+    y = (-weights[0] - weights[1] * x) / weights[2]
+    ax.plot(x, y)
+
+    plt.fill_between(x,y,y.min()-1,color='red',alpha=0.50)
+    plt.fill_between(x,y,y.max()+1,color='blue',alpha=0.30)
+    plt.xlim(-1.5,7)
+    plt.ylim(-1.5,7)
+    plt.xlabel('x1')
+    plt.ylabel('x2')
+    plt.show()
+    
+def plot_withoutline(datamatrix, labelmatrix):
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    dataarray = np.asarray(datamatrix)
+    n = dataarray.shape[0]
+
+    # Keep track of the two classes in different arrays so they can be plotted later...
+    xcord1 = []
+    ycord1 = []
+    xcord2 = []
+    ycord2 = []
+    for i in range(n):
+        if int(labelmatrix[i]) == 1:
+            xcord1.append(dataarray[i, 0])
+            ycord1.append(dataarray[i, 1])
+        else:
+            xcord2.append(dataarray[i, 0])
+            ycord2.append(dataarray[i, 1])
+    fig = plt.figure()
+
+    # Plot the data as points with different colours
+    ax = fig.add_subplot(111)
+    ax.scatter(xcord1, ycord1, s=30, c='red', marker='s')
+    ax.scatter(xcord2, ycord2, s=30, c='green')
+
+    plt.xlim(-1.5,7)
+    plt.ylim(-1.5,7)
+    plt.xlabel('x1')
+    plt.ylabel('x2')
+    plt.show()
+    
 class svm_basic:
 
     def __init__(self):
         self.C = 1
         self.toler = 0.001
         self.maxIter = 50
+        self.ws = None
+        self.b = None
+        self.listweights = []
 
-    # def fit(self, X, Y):
+    def fit(self, X, Y):
+        self.b, alphas = smoPK(X, Y, 0.6, 0.001, 40)
+        self.ws = calcWs(alphas, X, Y)
+#         listweights = []
+        self.listweights.append(self.b.getA()[0])
+        self.listweights.append(self.ws[0][0])
+        self.listweights.append(self.ws[1][0])
+#         listweights
+        plot_fit(self.listweights, X, Y)
+        return self.listweights
 
-    # def predict(self, X):
-
-    def loadDataSet(self, fileName):
-        dataMat = []; labelMat = []
-        fr = open(fileName)
-        for line in fr.readlines():
-            lineArr = line.strip().split(',')
-            dataMat.append([lineArr[0], lineArr[1]])
-            labelMat.append((lineArr[2]))
-        return dataMat,labelMat
-
-    def calcEkK(oS, k):
-        fXk = float(multiply(oS.alphas,oS.labelMat).T*(oS.X*oS.X[k,:].T)) + oS.b
-        Ek = fXk - float(oS.labelMat[k])
-        return Ek
-        
-    def selectJK(i, oS, Ei):         #this is the second choice -heurstic, and calcs Ej
-        maxK = -1; maxDeltaE = 0; Ej = 0
-        oS.eCache[i] = [1,Ei]  #set valid #choose the alpha that gives the maximum delta E
-        validEcacheList = nonzero(oS.eCache[:,0].A)[0]
-        if (len(validEcacheList)) > 1:
-            for k in validEcacheList:   #loop through valid Ecache values and find the one that maximizes delta E
-                if k == i: continue #don't calc for i, waste of time
-                Ek = calcEkK(oS, k)
-                deltaE = abs(Ei - Ek)
-                if (deltaE > maxDeltaE):
-                    maxK = k; maxDeltaE = deltaE; Ej = Ek
-            return maxK, Ej
-        else:   #in this case (first time around) we don't have any valid eCache values
-            j = selectJrand(i, oS.m)
-            Ej = calcEkK(oS, j)
-        return j, Ej
-
-    def updateEkK(oS, k):#after any alpha has changed update the new value in the cache
-        Ek = calcEkK(oS, k)
-        oS.eCache[k] = [1,Ek]
-            
-    def innerLK(i, oS):
-        Ei = calcEkK(oS, i)
-        if ((oS.labelMat[i]*Ei < -oS.tol) and (oS.alphas[i] < oS.C)) or ((oS.labelMat[i]*Ei > oS.tol) and (oS.alphas[i] > 0)):
-            j,Ej = selectJK(i, oS, Ei) #this has been changed from selectJrand
-            alphaIold = oS.alphas[i].copy(); alphaJold = oS.alphas[j].copy();
-            if (oS.labelMat[i] != oS.labelMat[j]):
-                L = max(0, oS.alphas[j] - oS.alphas[i])
-                H = min(oS.C, oS.C + oS.alphas[j] - oS.alphas[i])
+    def predict(self, X):
+        hypnew = []
+        dataMat = mat(X)
+        hyp = dataMat*mat(self.ws) + self.b
+        for i in range (len(X)):
+            if hyp[i] > 0:
+                hypnew.append(1)
             else:
-                L = max(0, oS.alphas[j] + oS.alphas[i] - oS.C)
-                H = min(oS.C, oS.alphas[j] + oS.alphas[i])
-            if L==H: print("L==H"); return 0
-            eta = 2.0 * oS.X[i,:]*oS.X[j,:].T - oS.X[i,:]*oS.X[i,:].T - oS.X[j,:]*oS.X[j,:].T
-            if eta >= 0: print("eta>=0"); return 0
-            oS.alphas[j] -= oS.labelMat[j]*(Ei - Ej)/eta
-            oS.alphas[j] = clipAlpha(oS.alphas[j],H,L)
-            updateEkK(oS, j) #added this for the Ecache
-            if (abs(oS.alphas[j] - alphaJold) < 0.00001): print("j not moving enough"); return 0
-            oS.alphas[i] += oS.labelMat[j]*oS.labelMat[i]*(alphaJold - oS.alphas[j])#update i by the same amount as j
-            updateEkK(oS, i) #added this for the Ecache                    #the update is in the oppostie direction
-            b1 = oS.b - Ei- oS.labelMat[i]*(oS.alphas[i]-alphaIold)*oS.X[i,:]*oS.X[i,:].T - oS.labelMat[j]*(oS.alphas[j]-alphaJold)*oS.X[i,:]*oS.X[j,:].T
-            b2 = oS.b - Ej- oS.labelMat[i]*(oS.alphas[i]-alphaIold)*oS.X[i,:]*oS.X[j,:].T - oS.labelMat[j]*(oS.alphas[j]-alphaJold)*oS.X[j,:]*oS.X[j,:].T
-            if (0 < oS.alphas[i]) and (oS.C > oS.alphas[i]): oS.b = b1
-            elif (0 < oS.alphas[j]) and (oS.C > oS.alphas[j]): oS.b = b2
-            else: oS.b = (b1 + b2)/2.0
-            return 1
-        else: 
-            return 0
-
-    def smoPK(self, dataMatIn, classLabels, C = 1, toler = 0.001, maxIter = 50):    #full Platt SMO
-        oS = optStructK(mat(dataMatIn),mat(classLabels).transpose(),C,toler)
-        iter = 0
-        entireSet = True; alphaPairsChanged = 0
-        while (iter < maxIter) and ((alphaPairsChanged > 0) or (entireSet)):
-            alphaPairsChanged = 0
-            if entireSet:   #go over all
-                for i in range(oS.m):        
-                    alphaPairsChanged += innerLK(i,oS)
-                    print("fullSet, iter: %d i:%d, pairs changed %d" % (iter,i,alphaPairsChanged))
-                iter += 1
-            else:#go over non-bound (railed) alphas
-                nonBoundIs = nonzero((oS.alphas.A > 0) * (oS.alphas.A < C))[0]
-                for i in nonBoundIs:
-                    alphaPairsChanged += innerLK(i,oS)
-                    print("non-bound, iter: %d i:%d, pairs changed %d" % (iter,i,alphaPairsChanged))
-                iter += 1
-            if entireSet: entireSet = False #toggle entire set loop
-            elif (alphaPairsChanged == 0): entireSet = True  
-            print("iteration number: %d" % iter)
-        return oS.b,oS.alphas
+                hypnew.append(-1)
+        return hypnew
         
-svm = svm_basic()
+if __name__ == '__main__':        
+    X, Y = loadDataSet('linearly_separable.csv')
 
-dataMat,labelMat = svm.loadDataSet("linearly_separable.csv")
-if(dataMat != 0):
-    print("Loaded")
+    split = int(len(Y) * 0.8)
+    train_X = X[:split]
+    train_Y = Y[:split]
+    test_X = X[split:]
+    test_Y = Y[split:]
 
-oS.b,oS.alphas = svm.smoPK(dataMat, labelMat)
+    svm = svm_basic()
+    listweights = svm.fit(train_X, train_Y)
 
-print(oS.b)
+    plot_withoutline(train_X, train_Y)
